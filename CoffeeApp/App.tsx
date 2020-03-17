0327c0677df28b6bd603ee63cd0dafd4bebd37e8
Alex Koppara <alexkoppara@Alexs-MBP.home>
2020-03-17 14:10:43 -0400

    getting started

---
+++ CoffeeApp/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CoffeeApp from './src';
+
+export default function App() {
+  return (
+    <CoffeeApp></CoffeeApp>
+  );
+}
